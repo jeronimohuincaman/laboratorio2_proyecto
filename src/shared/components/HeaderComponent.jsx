c2c381d8de2f4100d969bc389522e5bca65fd9ec
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2024-05-20 18:05:39 -0300

    Finalizacion estructura basica HomePage - Se creo componente de skills. - Se creo componente de footer. - Se agregaron imagenes. - Se termino maquetacion basica.

--- src/shared/components/HeaderComponent.jsx
+++ src/shared/components/HeaderComponent.jsx
@@ -1,13 +1,13 @@
-import { Menu } from "@mui/icons-material";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const HeaderComponent = () => {
     return (
-        <AppBar position="static">
+        <AppBar position="static" sx={{backgroundColor: "darkslategray"}}>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    <Menu />
+                    <AcUnitIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Mi portfolio
---
+++ src/shared/components/SkillsComponent.jsx
@@ -0,0 +1,62 @@
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+
+const skills = [
+  {
+    title: "Javascript",
+    description: "Conocimiento amplio en el lenguaje de POO mas utilizado en la actualidad.",
+    image: "/assets/javascript.jpg"
+  },
+  {
+    title: "Angular",
+    description: "Experiencia utilizando este framework para el desarrollo de diferentes sowtware de gestion.",
+    image: "/assets/angular.jpg"
+  },
+  {
+    title: "Nest JS",
+    description: "Experiencia en backend realizando APIS.",
+    image: "/assets/nestjs.svg"
+  },
+  {
+    title: "Yii2",
+    description: "Experiencia en backend realizando APIS con este framework de php.",
+    image: "/assets/yii2.jpg"
+  },
+  {
+    title: "Metodologia SCRUM",
+    description: "Se tiene experiencia trabajando en diversos equipos de trabajo con la metodologia agil SCRUM",
+    image: "/assets/scrum.jpg"
+  }
+]
+
+const SkillsComponent = () => {
+  return (
+    <Grid container spacing={4}>
+      {
+        skills.map((skill, index) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Card sx={{ backgroundColor: "lightgrey" }}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={skill.image}
+                  alt={skill.title}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {skill.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {skill.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          )
+        })
+      }
+    </Grid>
+  )
+};
+
+export default SkillsComponent;
